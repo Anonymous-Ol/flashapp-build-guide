742b11094b269efc0b33a27f65cd49303c565246
Anonymous-Ol <84883214+Anonymous-Ol@users.noreply.github.com>
2025-03-08 18:30:05 -0500

    Update temperature_converter.c

--- temperature_converter_example/temperature_converter.c
+++ temperature_converter_example/temperature_converter.c
@@ -14,7 +14,7 @@ FRAME(appObj, OO_SYSTEM_FRAME, 0, OO_APP_FLAGS, 4)
    ATTR(OO_APP_DEFAULT_MENU, &AppMenu )
 ENDFRAME
 
-volatile uint32_t test = 53;
+volatile long int test = 53;
 pFrame pAppObj = (pFrame)&appObj; // must be 1st!
 WINDOW appW;
 char buf_cels[11];
